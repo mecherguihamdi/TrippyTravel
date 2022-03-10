037a4e47a6f3f53327711a5e74c3947630d72ba6
4rch1tect <yassine.cherif@esprit.tn>
2022-03-10 14:51:54 +0100

    tri done

--- src/Controller/AttractionController.php
+++ src/Controller/AttractionController.php
@@ -14,9 +14,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Knp\Component\Pager\PaginatorInterface;
 
-    /**
-     * @Route("/")
-     */
+/**
+ * @Route("/")
+ */
 class AttractionController extends AbstractController
 {
     /**
@@ -24,22 +24,21 @@ class AttractionController extends AbstractController
      */
     public function index(Request $request, PaginatorInterface $paginator): Response
     {
-        $rep=$this->getDoctrine()->getRepository(Attraction::class);
-        
-        $attraction =$rep-> findAll();
+        $rep = $this->getDoctrine()->getRepository(Attraction::class);
+
+        $attraction = $rep->findAll();
         $attraction = $paginator->paginate(
             $attraction, // Requête contenant les données à paginer (ici nos articles)
             $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
             3 // Nombre de résultats par page
         );
-      
+
         return $this->render('attraction/index.html.twig', [
             'attraction' => $attraction,
-           
-        ]); 
-        
+
+        ]);
     }
-     /**
+    /**
      * @Route("/attraction_indexMap", name="attraction_indexMap", methods={"GET"})
      */
 
@@ -50,20 +49,20 @@ class AttractionController extends AbstractController
         ]);
     }
 
-//     /**
-//     * @Route("/attraction_indexDetail", name="attraction_indexDetail", methods={"GET"})
-//     */
-//
-//    public function indexDetail(AttractionRepository $AttractionRepository): Response
-//    {
-//        return $this->render('attraction/indexDetail.html.twig', [
-//            'attraction' => $AttractionRepository->findAll(),
-//        ]);
-//    }
+    //     /**
+    //     * @Route("/attraction_indexDetail", name="attraction_indexDetail", methods={"GET"})
+    //     */
+    //
+    //    public function indexDetail(AttractionRepository $AttractionRepository): Response
+    //    {
+    //        return $this->render('attraction/indexDetail.html.twig', [
+    //            'attraction' => $AttractionRepository->findAll(),
+    //        ]);
+    //    }
     /**
      * @Route("/attractionfront/{id}", name="attractionfront_single" , methods={"GET","POST"})
      */
-    public function attraction_one(EntityManagerInterface $entityManage,Request $request,Attraction $attraction): Response
+    public function attraction_one(EntityManagerInterface $entityManage, Request $request, Attraction $attraction): Response
     {
         $attractionservation = new AttractionReservation();
         $form = $this->createFormBuilder(null)
@@ -78,15 +77,13 @@ class AttractionController extends AbstractController
             $attractionservation->setPrix($attraction->getPrix());
             $attraction->addAttractionreservation($attractionservation);
             $entityManage->flush();
-
         }
         return $this->render('attraction/attractionreservation.html.twig', [
             'form' => $form->createView(),
         ]);
-
     }
 
-        /**
+    /**
      * @Route("/admin-dashboard/attraction", name="admin-dashboard/attraction")
      */
     public function admin_index(AttractionRepository $attractionRepository): Response
@@ -97,32 +94,44 @@ class AttractionController extends AbstractController
     }
 
     /**
-   * @Route("/searchA", name="ajax_searchA")
-   */
-public function searchAction(Request $request,AttractionRepository $repository)
-{
-    $em = $this->getDoctrine()->getManager();
-    $libelle = $request->get('q');
-    if($request->get('q')){
-        $attraction =$em->getRepository(Attraction::class)->findEntitiesByLibelle($libelle);
-        if(!$attraction ) {
-            $result['attraction ']['error'] = "Attraction introuvable !";
+     * @Route("/admin-dashboard/attraction/id_categorie/{id_categorie}", name="attraction_index_id_categorie", methods={"GET"})
+     */
+    public function indexByCategorie(Request $request, AttractionRepository $attractionRepository, String $id_categorie): Response
+    {
+
+        return $this->render('attraction/admin_index.html.twig', [
+            'attractions' => $attractionRepository->findBy(['id_categorie' => $id_categorie]),
+        ]);
+    }
+
+    /**
+     * @Route("/searchA", name="ajax_searchA")
+     */
+    public function searchAction(Request $request, AttractionRepository $repository)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $libelle = $request->get('q');
+        if ($request->get('q')) {
+            $attraction = $em->getRepository(Attraction::class)->findEntitiesByLibelle($libelle);
+            if (!$attraction) {
+                $result['attraction ']['error'] = "Attraction introuvable !";
+            } else {
+                $result['attraction '] = $this->getRealEntities($attraction);
+            }
         } else {
-            $result['attraction '] = $this->getRealEntities($attraction );
+            $result['attraction '] = $this->getRealEntities($repository->findAll());
         }
-    }else{
-        $result['attraction '] = $this->getRealEntities($repository->findAll());
+
+        return new Response(json_encode($result));
     }
 
-    return new Response(json_encode($result));
-}
-
-public function getRealEntities($attraction ){
-    foreach ($attraction  as $attraction ){
-        $realEntities[$attraction ->getId()] = [$attraction->getLibelle(),$attraction->getLocalisation(),$attraction->getHorraire(), $attraction->getPrix(), $attraction->getIdCategorie(), "uploads/images/attraction/".$attraction->getImage()];
+    public function getRealEntities($attraction)
+    {
+        foreach ($attraction  as $attraction) {
+            $realEntities[$attraction->getId()] = [$attraction->getLibelle(), $attraction->getLocalisation(), $attraction->getHorraire(), $attraction->getPrix(), $attraction->getIdCategorie(), "uploads/images/attraction/" . $attraction->getImage()];
+        }
+        return $realEntities;
     }
-    return $realEntities;
-}
 
     /**
      * @Route("/admin-dashboard/attraction/new", name="attraction_new", methods={"GET", "POST"})
@@ -154,7 +163,6 @@ public function getRealEntities($attraction ){
         return $this->render('attraction/show.html.twig', [
             'attraction' => $attraction,
         ]);
-
     }
 
     /**
@@ -182,7 +190,7 @@ public function getRealEntities($attraction ){
      */
     public function delete(Request $request, Attraction $attraction, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$attraction->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $attraction->getId(), $request->request->get('_token'))) {
             $entityManager->remove($attraction);
             $entityManager->flush();
         }
