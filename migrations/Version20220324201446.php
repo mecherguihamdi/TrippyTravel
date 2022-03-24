0ffc70657e5516a7d86271c9c14303c9376e9a55
amani-boussaa <98672961+amani-boussaa@users.noreply.github.com>
2022-03-24 21:40:15 +0100

    fix the branch

--- migrations/Version20220324201446.php
+++ migrations/Version20220324201446.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220311022652 extends AbstractMigration
+final class Version20220324201446 extends AbstractMigration
 {
     public function getDescription(): string
     {
